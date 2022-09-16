781eda8b09f97c7eee7cbb60525429af4043c193
rubhain <bugrahantufekcioglu@gmail.com>
2022-09-16 16:19:54 +0300

    Form component

--- contact-list/src/components/Contacts/Form/index.tsx
+++ contact-list/src/components/Contacts/Form/index.tsx
@@ -1,7 +1,48 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
-function Form() {
-  return <div>Form</div>;
+const initialFormValues = { fullname: "", phone_number: "" };
+function Form({ addContact, contacts }: { addContact: any; contacts: any }) {
+  const [form, setForm] = useState(initialFormValues);
+
+  useEffect(() => {
+    setForm(initialFormValues);
+  }, [contacts]);
+
+  const onChangeInput = (e: any) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = (e: any) => {
+    e.preventDefault();
+
+    if (form.fullname === "" || form.phone_number === "") {
+      return false;
+    }
+
+    addContact([...contacts, form]);
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <div>
+        <input
+          name="fullname"
+          placeholder="Full Name"
+          value={form.fullname}
+          onChange={onChangeInput}
+        />
+      </div>
+      <div>
+        <input
+          name="phone_number"
+          placeholder="Phone Number"
+          value={form.phone_number}
+          onChange={onChangeInput}
+        />
+      </div>
+      <button>Add</button>
+    </form>
+  );
 }
 
 export default Form;
--- contact-list/src/components/Contacts/index.tsx
+++ contact-list/src/components/Contacts/index.tsx
@@ -1,12 +1,23 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import List from "./List";
 import Form from "./Form";
 
+interface addContact {
+  fullname: any;
+  phone_number: any;
+}
+
 function Contacts() {
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    console.log(contacts);
+  }, [contacts]);
+
   return (
     <div>
       <List />
-      <Form />
+      <Form addContact={setContacts} contacts={contacts} />
     </div>
   );
 }
