386c713a58efe8a7873a3e4645d623552ff74680
rubhain <bugrahantufekcioglu@gmail.com>
2022-09-16 15:01:32 +0300

    Initialize project using Create React App

--- contact-list/src/components/Contacts/index.tsx
+++ contact-list/src/components/Contacts/index.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import List from "./List";
+import Form from "./Form";
 
 function Contacts() {
-  return <div>Contacts</div>;
+  return (
+    <div>
+      <List />
+      <Form />
+    </div>
+  );
 }
 
 export default Contacts;
