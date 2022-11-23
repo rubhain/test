c30fec2c8a11bbb15594e24d67c0c8d82ef7f97b
rubhain <bugrahantufekcioglu@gmail.com>
2022-11-23 12:32:41 +0300

    Context

--- weather-app-react-ts/src/context/WeatherContext.tsx
+++ weather-app-react-ts/src/context/WeatherContext.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { createContext, SetStateAction, useEffect, useState } from "react";
 import cities from "../cities_of_turkey.json";
-//https://api.weatherbit.io/v2.0/forecast/daily?/&days=7&lat=38.123&lon=-78.543&key=67c2dbf8ee214aaab7980865a6c4fb32
+
 type WeatherContextType = {
   city: any;
   setCity: React.Dispatch<React.SetStateAction<{}>>;
