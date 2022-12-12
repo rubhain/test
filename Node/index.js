96db8278858bbf867d542c9e95fcb57d1a5887ea
rubhain <bugrahantufekcioglu@gmail.com>
2022-12-12 16:27:06 +0300

    .

--- Node/index.js
+++
@@ -1,5 +0,0 @@
-import hello, { sum, subtraction } from "./my-module.js";
-
-console.log(sum(3, 5));
-console.log(subtraction(3, 5));
-hello("Bugri");
--- Node/my-module.js
+++
@@ -1,9 +0,0 @@
-const sum = (a, b) => a + b;
-const subtraction = (a, b) => a - b;
-
-const hello = (name) => {
-  console.log(`Hello ${name}`);
-};
-
-export { sum, subtraction };
-export default hello;
